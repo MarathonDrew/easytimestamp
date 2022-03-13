3daf1e965af062d02a97045a927fed44b0e946f2
Drew Hinderhofer <drew.hinderhofer@gmail.com>
2022-03-12 22:21:26 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function HomePage() {
-    return <div>Reese's Peanut Butter cups are the goat</div>
+    return (
+        <div>
+            Reese's Peanut Butter cups are the goat. Justin's cups are okay
+        </div>
+    )
 }
 
 export default HomePage
